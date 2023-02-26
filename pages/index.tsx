3f31880ed54dd4be3c3f69ef9b277182d62ff3ea
tosaken1116 <nagato20021122@gmail.com>
2023-02-26 23:33:26 +0900

    add:ログの追加 #9

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,6 +118,7 @@ export default function Home() {
     useEffect(() => {
         setLinkProps(localStorageLinks);
     }, [localStorageLinks]);
+    console.log(LinkProps);
     if (LinkProps == undefined) {
         return <CircularProgress />;
     } else {
